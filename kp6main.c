9edb0e419755a2cf2803f11427cf06b6c1483e4b
tesla-2002 <tesla_2002@mail.ru>
2023-09-19 03:16:56 +0300

    Update kp6main.c

--- kp6main.c
+++ kp6main.c
@@ -9,42 +9,62 @@ int main(int argc, char* argv[]) {
 	}
 
 	FILE* filetxt = fopen(argv[1], "a+");
-	FILE* filebin = fopen(argv[2], "a+");
+	FILE* filebin = fopen(argv[2], "a+b");
 
 	if (!filetxt || !filebin) {
 		printf("Не удалось открыть один из файлов\n");
 		exit(1);
 	}
-	
-	int index = 0, size = 0;
-	Passenger* pass = (Passenger*)malloc(sizeof(**pass));
-	pass[index] = (Passenger)malloc(sizeof(**pass));
-    while (passenger_read_bin(pass[index], filebin)) {
-   		index++;
-   		size++;
-   		pass[index] = (Passenger)malloc(sizeof(**pass));
-   		pass = (Passenger*)realloc(pass, (size+1) * sizeof(**pass));
-    } 
-
-    free(pass[index]);
-    pass = (Passenger*)realloc(pass, size * sizeof(**pass));
 
     int flag = 0;
     while (1) {
-    	printf("Что Вы хотите сделать?\n [1]-Вывести содержимое бинарного файла\n [2]-Добавить в текстовый файл\n\
- [3]-Добавить в бинарный файл\n [4]-Удалить из текстового файла\n [5]-Удалить из бинарного файла\n\
- [6]-Поиск пассажира по условию варианта\n [0]-Завершить\n");
+    	printf("Что Вы хотите сделать?\n [1]-Вывести содержимое бинарного файла\n\
+ [2]-Вывести содержимое текстового файла\n [3]-Добавить в текстовый файл\n\
+ [4]-Добавить в бинарный файл\n [5]-Удалить из текстового файла\n [6]-Удалить из бинарного файла\n\
+ [7]-Поиск пассажира по условию варианта\n [0]-Завершить\n");
     	scanf("%d", &flag);
     	switch(flag) {
-    		case PRINT:
-    			passenger_print(pass, size);
+    		case PRINTBIN:
+    			fclose(filebin);
+    			filebin = fopen(argv[2], "a+b");
+    			if (!filebin) {
+					printf("Не удалось открыть файл\n");
+					exit(1);
+				}
+
+    			passenger_print_from_bin(filebin);
+    			break;
+
+    		case PRINTTXT:
+    			fclose(filetxt);
+    			filetxt = fopen(argv[1], "a+");
+    			if (!filetxt) {
+					printf("Не удалось открыть файл\n");
+					exit(1);
+				}
+
+    			passenger_print_from_txt(filetxt);
     			break;
 
     		case ADDTOTXT:
+    			fclose(filetxt);
+    			filetxt = fopen(argv[1], "a+");
+    			if (!filetxt) {
+					printf("Не удалось открыть файл\n");
+					exit(1);
+				}
+				
     			passenger_add_to_txt(filetxt);
     			break;
 
     		case ADDTOBIN:
+    			fclose(filebin);
+    			filebin = fopen(argv[2], "a+b");
+    			if (!filebin) {
+					printf("Не удалось открыть файл\n");
+					exit(1);
+				}
+
     			passenger_add_to_bin(filebin);
     			break;
 
@@ -63,17 +83,31 @@ int main(int argc, char* argv[]) {
 				break;
 
 			case SEARCH:
+				int index = 0, size = 0;
+				Passenger* pass = (Passenger*)malloc(sizeof(**pass));
+				pass[index] = (Passenger)malloc(sizeof(**pass));
+				rewind(filebin);
+    			while (passenger_read_bin(pass[index], filebin)) {
+   					index++;
+   					size++;
+   					pass[index] = (Passenger)malloc(sizeof(**pass));
+   					pass = (Passenger*)realloc(pass, (size+1) * sizeof(**pass));
+    			} 
+
+    			free(pass[index]);
+    			pass = (Passenger*)realloc(pass, size * sizeof(**pass));
+
 				int ind = search_max(pass,size);
 				if (ind!=-1) {
-					printf("\nДа, такой пассажир есть: %s\t%s\n", pass[ind]->surname, pass[ind]->initials);
+					printf("\nДа, такой пассажир есть: %s %s \n", pass[ind]->surname, pass[ind]->initials);
 				} else {
 					printf("\nТакого пассажира нет в списке.\n");
 				}
 
+				pass_free(pass,size);
 				break;
 
 			default:
-				pass_free(pass,size);
 				fclose(filetxt);
 				fclose(filebin);
 				return 0;
