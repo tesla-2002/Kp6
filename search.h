6ec07af4a9babd8dc56cd5f73d32e13d6aab63a7
tesla-2002 <tesla_2002@mail.ru>
2023-09-19 03:17:02 +0300

    Update search.h

--- search.h
+++ search.h
@@ -5,19 +5,20 @@
 #include "passenger.h"
 
 typedef enum {
-    PRINT = 1,
-    ADDTOTXT = 2,
-    ADDTOBIN = 3,
-    DELETEFROMTXT = 4,
-    DELETEFROMBIN = 5, 
-    SEARCH = 6
+    PRINTBIN = 1,
+    PRINTTXT = 2,
+    ADDTOTXT = 3,
+    ADDTOBIN = 4,
+    DELETEFROMTXT = 5,
+    DELETEFROMBIN = 6, 
+    SEARCH = 7
 } var_flags;
 
 Passenger passenger_get_from_user (Passenger pass);
 void passenger_add_to_txt (FILE* filetxt);
 void passenger_add_to_bin (FILE* filebin);
-void passenger_delete_from_txt (const char* filename, const char* sname, FILE* filetxt);
-void passenger_delete_from_bin (const char* filename, const char* sname, FILE* filebin); 
+void passenger_delete_from_txt (const char* filename, const char* passname, FILE* filetxt);
+void passenger_delete_from_bin (const char* filename, const char* passname, FILE* filebin); 
 void pass_free(Passenger* pass, int size);
 int search_max (Passenger* pass, int size);
 
